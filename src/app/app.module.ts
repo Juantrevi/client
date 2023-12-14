4896c481eb26c132de59ff92420a3336de08e5bf
Juantrevi <juantrevi70@gmail.com>
2023-12-14 12:50:32 +0000

    --Initial creation--

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/**
+ * @NgModule decorator with the declarations and imports properties.
+ * The declarations property tells Angular which components belong to the module.
+ * The imports property tells Angular which other modules the app needs.
+ */
+
 @NgModule({
   declarations: [
     AppComponent
