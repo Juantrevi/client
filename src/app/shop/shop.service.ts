7f1ac2f421f259d460dd1691793ac14ae11fd200
Juantrevi <juantrevi70@gmail.com>
2023-12-22 23:55:28 +0000

    --providedIn explanation --

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * @Injectable decorator 
+ * The @Injectable decorator marks it as a service that can be injected
+ * as a dependency, so we can inject our service into our components.
+ */
 @Injectable({
+  /** 
+   * providedIn:
+   * This means that the service will be provided in the root module
+   * and will be instantiated when the application loads.
+   * We could change it, for example, to providedIn: ShopModule
+   * and the service would be instantiated when the ShopModule loads.
+   * If we keep it in 'root', it will keep alive until the application is closed
+   * This makes services ideal to store application wide state, something that
+   * component are not designed to do.
+  */
   providedIn: 'root'
 })
 export class ShopService {
