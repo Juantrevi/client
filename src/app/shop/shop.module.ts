c6489df355c5481e330bb0f764e9281e25d563d0
Juantrevi <juantrevi70@gmail.com>
2023-12-27 00:24:16 +0000

    -- Structuring for routing --

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 /**
  * ShopModule
@@ -13,7 +14,8 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
